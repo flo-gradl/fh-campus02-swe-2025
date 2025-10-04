becbf7e33bd6e5e56c5b98ad9cf7a09eadc9ace7
Simon <s44650@edu.campus02.at>
2025-10-04 13:27:51 +0200

    Added new E2E Test

--- src/test/java/at/campus02/swe/E2ETest/E2ETest1.java
+++ src/test/java/at/campus02/swe/E2ETest/E2ETest1.java
@@ -1,4 +1,22 @@
 package at.campus02.swe.E2ETest;
 
+import at.campus02.swe.logic.CalculatorImpl;
+import at.campus02.swe.parser.Parser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
 public class E2ETest1 {
+
+    @Test
+    public void testModuloOperation() throws Exception {
+
+        CalculatorImpl calc = new CalculatorImpl();
+        Parser par = new Parser(calc);
+
+        double result = par.parse(new File("src/test/resources/test06.xml"));
+
+        Assert.assertEquals(7.0, result, 0);
+    }
 }
