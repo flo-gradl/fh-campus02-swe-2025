996f29458919dec07defd00932dfe8d449ae6bbf
Max <maxtiefengraber@proton.me>
2025-11-07 18:46:15 +0100

    E2ETestCos

---
+++ src/test/java/at/campus02/swe/E2ETest/E2ETestCos.java
@@ -0,0 +1,21 @@
+package at.campus02.swe.E2ETest;
+
+import at.campus02.swe.logic.CalculatorImpl;
+import at.campus02.swe.parser.Parser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class E2ETestCos {
+    @Test
+    public void testSubDivideOperation() throws Exception {
+
+        CalculatorImpl calc = new CalculatorImpl();
+        Parser par = new Parser(calc);
+
+        double result = par.parse(new File("src/test/resources/test08.xml"));
+
+        Assert.assertEquals(0.154, result, 0.01);
+    }
+}
